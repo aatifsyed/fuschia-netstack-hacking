19ca4d9ccabb4a266c050e1beb063cc001323511
Aatif Syed <aatifsyedyp@gmail.com>
2024-02-05 16:25:22 +0000

    feat: downloader script

---
+++ download.bash
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -o errexit -o xtrace -o pipefail -o nounset
+
+# See https://fuchsia.googlesource.com/fuchsia/+/refs/heads/main
+# when=refs/heads/main
+when=816672d4d051a2222b63cd3173b3b5893d6ca169 # Fri Feb 02 17:08:57 2024 +0000
+
+declare -A folder_src_path
+folder_src_path=(
+    # see also Cargo.toml::workspace::members
+    [const-unwrap]=src/lib/const-unwrap
+    [explicit]=src/connectivity/network/lib/explicit
+    [fakestd]=src/connectivity/network/netstack3/core/fakestd
+    [internet-checksum]=src/connectivity/lib/internet-checksum
+    [net-types]=src/connectivity/lib/net-types
+    [packet-formats]=src/connectivity/lib/packet-formats
+    [packet]=src/lib/network/packet
+)
+
+rm -rf upstream
+mkdir upstream
+pushd upstream
+
+    for folder in "${!folder_src_path[@]}"
+    do
+        mkdir --parents "$folder"
+        pushd "$folder"
+            find . -not -name 'Cargo.toml' -delete
+            curl --fail "https://fuchsia.googlesource.com/fuchsia/+archive/$when/${folder_src_path[$folder]}.tar.gz" \
+                | tar --extract --gzip --file - --verbose
+        popd
+    done
+
+    curl --fail "https://fuchsia.googlesource.com/fuchsia/+/$when/LICENSE?format=TEXT" \
+        | base64 --decode \
+        > LICENSE
+
+    curl --fail "https://fuchsia.googlesource.com/fuchsia/+/$when/rustfmt.toml?format=TEXT" \
+        | base64 --decode \
+        > rustfmt.toml
+
+popd # upstream
